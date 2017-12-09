18a1a738d61b8845cc75a16c2a5f2700d5a771ff
TUT3Software4Project <tut.umemura.lab@gmail.com>
2017-12-09 22:32:07 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# InformationQuantity
+# InformationQuantity
---
+++ s4/b173352/Hello.java
@@ -0,0 +1,8 @@
+package s4.b173352;
+import java.lang.*;
+
+public class Hello {
+    public static void main(String[] args) {
+	System.out.println("hello..(buggy)");
+    }
+}
